b13d103402c1fbbfde142f811ce006ec7eb6d870
James Booth <boothj5@gmail.com>
2017-02-04 17:22:21 +0000

    Add comments

--- hackcheck.sh
+++ hackcheck.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# first argument is properties file with
+#
+# FTP properties
+#   user=ftp@myserver.com
+#   server=192.168.0.0.10
+#
+# Local folder to store downloaded contents
+#   folder=myserver
+#
+# Access logs to check
+#   year=2017
+#   month=Feb
+
 . $1
 
 echo
@@ -31,7 +44,7 @@ echo Scanning for POST requests...
 grep POST $folder/logfiles/* > $folder/posts.log
 POST_COUNT=$(wc -l $folder/posts.log | cut -d' ' -f1)
 if [ $POST_COUNT -gt 0 ]; then
-    echo !! Found POST requests, see $folder/posts.log !!
+    echo !! Found $POST_COUNT POST requests, see $folder/posts.log !!
 else
     echo No POST requests found
 fi
