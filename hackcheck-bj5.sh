13f7ee7ce24e1a52cedb9a9675a6ef3d7986389e
James Booth <boothj5@gmail.com>
2017-02-04 01:58:19 +0000

    Add profanity site

---
+++ hackcheck-bj5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo
+read -s -p "Enter password: " PASS
+export SSHPASS=$PASS
+
+echo
+echo
+echo Removing previous contents...
+rm -rf public_html
+rm -rf logfiles
+
+echo
+echo Downloading contents...
+sshpass -e scp -r ftp@boothj5.com@94.136.40.103:/public_html .
+
+echo
+echo Looking for php files...
+find public_html -name \*.php
+
+echo
+echo Downloading access log...
+mkdir logfiles
+sshpass -e scp ftp@boothj5.com@94.136.40.103:/logfiles/*-$1-*.log logfiles/.
+
+echo
+echo Scanning for POST requests...
+ack POST logfiles
